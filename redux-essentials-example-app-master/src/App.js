d7a55831c364a1e910cf2f764804d5fb2e4b61f4
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2024-03-04 11:00:24 +0300

    new:dev: misc

--- redux-essentials-example-app-master/src/App.js
+++ redux-essentials-example-app-master/src/App.js
@@ -12,6 +12,9 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
+import { NotificationList } from './features/notifications/NotificationList'
 
 
 function App() {
@@ -30,16 +33,11 @@ function App() {
               </>
             )}
           />
-          <Route 
-            exact 
-            path="/posts/:postId"
-            component={SinglePostPage}
-          />
-          <Route
-            exact
-            path="/editPost/:postId"
-            component={EditPostForm}
-          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/notifications" component={NotificationList} />
           <Redirect to="/" />
         </Switch>
       </div>
